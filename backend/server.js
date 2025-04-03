6e7fd0c0c56498dff423b6b31366a46be42e91ec
Acharaf-Dev <contact.devlopeur@gmail.com>
2025-04-03 04:38:07 +0200

    fix test bugs

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const cors = require('cors');
 const express = require('express');
-const connectDB = require('./src/db');
+const {connectDB} = require('./src/db');
 
 const authRoutes = require('./src/routes/authRoute');
 const userRoutes = require('./src/routes/userRoute');
@@ -11,26 +11,30 @@ const contactRoutes = require('./src/routes/contactRoute');
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-connectDB();
+const serverInit = async () => {
+  await connectDB();
 
-// Middlewares
-app.use(express.json());
-app.use(cors());
+  // Middlewares
+  app.use(express.json());
+  app.use(cors());
 
-// Routes
-app.get('/api/hello', (req, res) => {
-  res.status(200).json({ message: 'Hello, World!' });
-});
-
-app.use('/auth', authRoutes);
-app.use('/user', userRoutes);
-app.use('/game', gameRoutes);
-app.use('/contact', contactRoutes);
-
-if (process.env.NODE_ENV !== 'test') {
-  app.listen(PORT, () => {
-    console.log(`🚀 Serveur lancé sur port ${PORT} derrière Traefik`);
+  // Routes
+  app.get('/api/hello', (req, res) => {
+    res.status(200).json({ message: 'Hello, World!' });
   });
-}
+
+  app.use('/auth', authRoutes);
+  app.use('/user', userRoutes);
+  app.use('/game', gameRoutes);
+  app.use('/contact', contactRoutes);
+
+  if (process.env.NODE_ENV !== 'test') {
+    app.listen(PORT, () => {
+      console.log(`🚀 Serveur lancé sur port ${PORT} derrière Traefik`);
+    });
+  }
+};
+
+serverInit(); // démarre la config et connexion DB
 
 module.exports = app;
