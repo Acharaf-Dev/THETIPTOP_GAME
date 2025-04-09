3e7fc88e0ca1eb56f4d857be486171cee1038459
Acharaf-Dev <contact.devlopeur@gmail.com>
2025-04-09 16:07:34 +0200

    fix bug

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,11 +1,17 @@
 module.exports = {
-    testEnvironment: "node",
-    collectCoverage: true,
-    collectCoverageFrom: [
-      "src/**/*.{js,jsx}",
-      "!src/index.js" // Exclut le fichier d'entrée si tu le souhaites
-    ],
-    coverageDirectory: "coverage",
-    coverageReporters: ["json", "lcov", "text", "clover"],
-  };
-  
+  testEnvironment: "node",
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/**/*.{js,jsx}",
+    "!src/index.js"
+  ],
+  coverageDirectory: "coverage",
+  coverageReporters: ["json", "lcov", "text", "clover"],
+  reporters: [
+    "default",
+    ["jest-junit", {
+      outputDirectory: "coverage",
+      outputName: "test-report.xml"
+    }]
+  ]
+};
