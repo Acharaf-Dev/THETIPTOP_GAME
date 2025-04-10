c5067dccf46710a07fcc055d8d6401bb89f8cd74
Acharaf-Dev <contact.devlopeur@gmail.com>
2025-04-10 22:47:47 +0200

    fix contact bug

--- backend/src/routes/authRoute.js
+++ backend/src/routes/authRoute.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { authMiddleware } = require("../middlewares/authMiddleware");
-const { registerUserController, loginUserController, logoutController} = require("../controllers/authController");
+const { registerUserController, loginUserController, logoutController, requestPasswordReset, resetPasswordController, changePasswordController } = require("../controllers/authController");
 
 
 //register user
@@ -12,5 +12,11 @@ router.post('/login', loginUserController);
 
 //logout user
 router.post('/logout', authMiddleware, logoutController);
+
+router.post('/request-reset', requestPasswordReset);
+
+router.post('/reset-password', resetPasswordController);
+
+router.post('/change-password', authMiddleware, changePasswordController);
 
 module.exports = router;
